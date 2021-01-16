8f7e5bec780c34dc93536bc7aab7fd5b969b905b
James J. Cook <jmsck55@gmail.com>
2021-01-16 14:54:30 -0500

    Delete ed_macro_named_debug.bat

--- ed_macro_named_debug.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Copyright (c) 2020 James Cook
-IF NOT "%EUDIR%"=="" GOTO label
-set EUDIR=%ONEDRIVE%\euphoria40
-set path=%EUDIR%\bin;%path%
-:label
-eui -D DEBUG -D USE_CONTROL_KEYS ed_macro_named.ex %*
