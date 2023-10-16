5693fcee475d276b824e168061cd130c5a34c53a
James <jmsck55@gmail.com>
2023-10-16 13:10:32 -0400

    bug fix

--- ed_macro_named.ex
+++ ed_macro_named.ex
@@ -2983,7 +2983,8 @@ procedure get_escape(boolean help)
 		if command[1] = GET_SUCCESS then -- special cases
 			if length(command[2]) = 1 then -- will not be zero.
 				answer = sprintf("\\x%02x", command[2]) -- command[2] length is one (1).
-			elsif length(command[2]) <= 16 then
+			else
+			-- elsif length(command[2]) <= 32 then
 				command = command[2]
 				set_top_line("little endian [reversed: 0xabcd to {#cd, #ab]? ")
 				if find('y', key_gets("yn", {})) then
