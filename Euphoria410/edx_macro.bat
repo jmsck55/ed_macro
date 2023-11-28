29ce4494aa901be6b1a340978ac7fa744cee8d4f
James <jmsck55@gmail.com>
2023-11-28 11:54:18 -0500

    copyrighted to help you, because of a dream I had.

--- Euphoria410/edx_macro.bat
+++ Euphoria410/edx_macro.bat
@@ -1,5 +1,5 @@
 @echo off
-rem Copyright 2023 James Cook
+rem Copyright (c) 2023 James Cook
 rem 
 rem Licensed under the Apache License, Version 2.0 (the "License");
 rem you may not use this file except in compliance with the License.
